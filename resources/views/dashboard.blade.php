e8fbdd536a59978a14bf31bd53dc696a35946a4c
Danh <danh.nguyen@appscyclone.com>
2023-03-19 12:02:43 +0700

    #< ROYAL-APP >: #Add table style for list authors

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,11 +12,30 @@
                     {{ __("You're logged in!") }}
                 </div>
 
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                @foreach ($authors->items as $author)
-                    <p>This is user {{ $author->first_name }}</p>
-                @endforeach
-                </div>
+                <table class="border-collapse table-auto w-full text-sm">
+                    <thead>
+                        <tr>
+                        <th class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Id</th>
+                        <th class="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">First Name</th>
+                        <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Last Name</th>
+                        <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Birthday</th>
+                        <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Gender</th>
+                        <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Place of Birth</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white dark:bg-slate-800">
+                        @foreach ($authors->items as $author)
+                            <tr>
+                                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $author->id }}</td>
+                                <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{{ $author->first_name }}</td>
+                                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">{{ $author->last_name }}</td>
+                                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $author->birthday }}</td>
+                                <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{{ $author->gender }}</td>
+                                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">{{ $author->place_of_birth }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
