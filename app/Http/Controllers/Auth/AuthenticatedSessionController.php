61f324937722825b9c141a7ff6596030ae333591
Danh <danh.nguyen@appscyclone.com>
2023-03-19 11:20:33 +0700

    #< ROYAL-APP >: #Update user's symphony skeleton api token

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,9 +9,12 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use GuzzleHttp\Client;
+use App\Models\User;
 
 class AuthenticatedSessionController extends Controller
 {
+    const SYM_SKELETON_API_URL = 'https://symfony-skeleton.q-tests.com/api/v2/token';
     /**
      * Display the login view.
      */
@@ -26,7 +29,8 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
-
+        // get symphony skeleton api and update user's sym_token
+        $this->saveSymSklApiToken($request);
         $request->session()->regenerate();
 
         return redirect()->intended(RouteServiceProvider::HOME);
@@ -44,5 +48,29 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/');
+    }
+
+    private function saveSymSklApiToken($request){
+        try {
+            $client = new Client();
+            $res = $client->post(self::SYM_SKELETON_API_URL, [
+                'json' => [
+                    'email' => config('core.SYM_SKL_API_USER'),
+                    'password' => config('core.SYM_SKL_API_PASSWORD')
+                ]
+            ]);
+            if($res->getStatusCode() == '200'){
+                $user = User::where([
+                    'email' => $request->email
+                ])->first();
+                $resData = json_decode($res->getBody());
+                if(!empty($resData->token_key) && !empty($user)){
+                    $user->sym_token = $resData->token_key;
+                    $user->save();
+                }
+            }
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+        }
     }
 }
---
+++ config/core.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    |--------------------------------------------------------------------------
+    | Symphony Skeleton API Authentication Credentials
+    |--------------------------------------------------------------------------
+*/
+return [
+    'SYM_SKL_API_USER' => 'ahsoka.tano@q.agency',
+    'SYM_SKL_API_PASSWORD' => 'Kryze4President'
+];
